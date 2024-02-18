625ef0186e581eafdabe34d8ebf73f8bb7e852f0
Екатерина <ekaterina.s3006@yandex.ru>
2024-02-18 19:37:28 +0300

    24.2 N2

--- members/models.py
+++ members/models.py
@@ -9,3 +9,18 @@ class CustomUser(AbstractUser):
     city = models.CharField(max_length=100)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
+
+class Payment(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    date_payment = models.DateField()
+    course = models.ForeignKey('materials.Course', related_name='course_payments', on_delete=models.CASCADE,
+                               null=True, blank=True)
+    lesson = models.ForeignKey('materials.Lesson', related_name='lesson_payments', on_delete=models.CASCADE,
+                               null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=20, choices=[('cash', 'Наличные'), ('transfer', 'Перевод')])
+
+    def __str__(self):
+        return f"{self.user} - {self.date} - {self.amount} - {self.payment_method}"
+
+
