9ac24b2be841e3346bc3b41d255249ee9d795739
Екатерина <ekaterina.s3006@yandex.ru>
2024-02-18 19:19:00 +0300

    24.2 N1

--- materials/serializers.py
+++ materials/serializers.py
@@ -8,6 +8,13 @@ class LessonSerializer(serializers.ModelSerializer):
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    # Создаем поле 'lessons_count', которое будет выводить количество уроков для каждого курса
+    lessons_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lessons_count(self, obj):
+        # Получаем количество уроков для данного курса
+        return obj.lesson_set.count()
