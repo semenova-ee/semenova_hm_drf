e11359fd833bffde410b51546161df92158a836a
Екатерина <ekaterina.s3006@yandex.ru>
2024-04-12 11:28:10 +0300

    hm 25.1 add group moderators

--- materials/validators.py
+++ materials/validators.py
@@ -1,16 +1,13 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-DISALLOWED_DOMAINS = [
-    'linkedin.com',
-    'tumblr.com',
-    'tilda.cc',
-    'wordpress.com',
+ALLOWED_DOMAINS = [
+    'youtube.com',
 ]
 
 def validate_allowed_domains(value):
-    for domain in DISALLOWED_DOMAINS:
-        if domain in value:
+    for domain in ALLOWED_DOMAINS:
+        if value != domain:
             raise ValidationError(
                 _(f'Links to {domain} are not allowed.'),
                 params={'value': value},
