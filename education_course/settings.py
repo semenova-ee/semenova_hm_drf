f65adc01d62a730ae1e396c05fae661fd9306513
Екатерина <ekaterina.s3006@yandex.ru>
2024-04-22 20:37:32 +0300

    bugs fixed

--- education_course/settings.py
+++ education_course/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'education_course.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER':os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
